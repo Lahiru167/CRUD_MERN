9ed17d0111fde2a35c37337288671b7f0d2db099
Lahiru167 <lahirushehan2002@gmail.com>
2025-05-15 15:15:54 +0530

    first commit

---
+++ Server/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+// Middleware
+app.use("/", (req, res, next) => {
+  res.send("it is working");
+});
+
+mongoose
+  .connect("mongodb+srv://admin:p1qRoOli0hZxYWVD@cluster0.bofqi.mongodb.net/")
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .then(() => {
+    app.listen(3000, () => {
+      console.log(`Server is running on http://localhost:${3000}`);
+    });
+  })
+  .catch((err) => console.log(err));
